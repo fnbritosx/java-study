04e2325a31fb28b600d65c422175a5f5d5a5f6f2
fnbritosx <fnbritosx@gmail.com>
2025-08-20 11:53:36 -0300

    feat: add conditional structures example

--- maratona-java/devbrito/maratonajava/introducao/Class05ConditionalStructures01.java
+++ maratona-java/devbrito/maratonajava/introducao/Class05ConditionalStructures01.java
@@ -1,6 +1,6 @@
 package devbrito.maratonajava.introducao;
 
-public class Class05ConditionalStructures {
+public class Class05ConditionalStructures01 {
     public static void main(String[] args) {
         int age = 15;
         boolean isAllowedToBuyAlcohol = age >= 18;
--- maratona-java/devbrito/maratonajava/introducao/Class05ConditionalStructures02.java
+++ maratona-java/devbrito/maratonajava/introducao/Class05ConditionalStructures02.java
@@ -1,4 +1,21 @@
 package devbrito.maratonajava.introducao;
 
 public class Class05ConditionalStructures02 {
+    public static void main(String[] args) {
+        // age < 15  → children's category
+        // age >= 15 && age < 18 → youth category
+        // agr >= 18 → adult category
+
+        int age = 45;
+        String category;
+
+        if (age < 15){
+            category = "Children's category!";
+        } else if (age >= 15 && age < 18){
+            category = "Youth category!";
+        } else{
+            category = "Adult category!";
+        }
+        System.out.println(category);
+    }
 }
