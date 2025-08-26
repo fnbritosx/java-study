aa96363a358248c15639dffa380bb4c8f5bb066f
fnbritosx <fnbritosx@gmail.com>
2025-08-26 12:17:13 -0300

    chore: rename Class02PrimitiveTypesExercise filename

---
+++ maratona-java/devbrito/maratonajava/introducao/Class02PrimitiveTypesExercise.java
@@ -0,0 +1,20 @@
+package devbrito.maratonajava.introducao;
+/*
+PRACTICE
+
+create variables for the fields described below between <> and print the following message:
+I <name>, living in address <address>,
+I confirm that I received the salary $<salary> on date, date>
+ */
+public class Class02PrimitiveTypesExercise {
+    public static void main(String[] args) {
+        String name = "Fernando";
+        String address = "Road Miltan da Cunha Vaz";
+        double salary = 2354.64;
+        String date = "15/08/2025";
+        String report = String.format("I %s, living in address %s %nI confirm that I received the salary $%.2f on date, %s", name,
+        address, salary, date);
+
+        System.out.printf(report);
+    }
+}
