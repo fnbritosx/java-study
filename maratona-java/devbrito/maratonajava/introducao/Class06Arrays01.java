35a8e04e46065f489060a0a88d96dbde08bdb030
fnbritosx <fnbritosx@gmail.com>
2025-08-26 12:32:18 -0300

    feat: exercises on arrays

---
+++ maratona-java/devbrito/maratonajava/introducao/Class06Arrays01.java
@@ -0,0 +1,17 @@
+package devbrito.maratonajava.introducao;
+
+public class Class06Arrays01 {
+    public static void main(String[] args) {
+        // In arrays, primitive data types become references.
+        // syntax to create an array:
+        // data_type[] variableName = new data_type[memorySize];
+
+        int[] ages = new int[3];
+
+        ages[0] = 19;
+        ages[1] = 14;
+        ages[2] = 4;
+
+        System.out.println(ages[0]);
+    }
+}
