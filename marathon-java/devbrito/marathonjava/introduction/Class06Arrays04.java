cb912cd1485bef7bbac8f354ad07d4cf0c7100ef
fnbritosx <fnbritosx@gmail.com>
2025-08-27 12:55:56 -0300

    feat: exercises on arrays

---
+++ marathon-java/devbrito/marathonjava/introduction/Class06Arrays04.java
@@ -0,0 +1,12 @@
+package devbrito.maratonajava.introducao;
+
+public class Class06Arrays04 {
+    public static void main(String[] args) {
+        // Iterating through an array using a for-each loop
+        String[] name = {"Irelia", "Yasuo", "Yone"};
+
+        for(String mains : name){
+            System.out.println(mains);
+        }
+    }
+}
