1d5c77f73388a3cc6faa6ee0a54b74b876d6fc96
fnbritosx <fnbritosx@gmail.com>
2025-09-08 15:45:38 -0300

    chore: add comments to clarify code logic

--- marathon-java/devbrito/marathonjava/javacore/introduction/initializationblock/domain/Anime.java
+++ marathon-java/devbrito/marathonjava/javacore/introduction/initializationblock/domain/Anime.java
@@ -3,7 +3,10 @@ package devbrito.marathonjava.javacore.introduction.initializationblock.domain;
 public class Anime {
     private String name;
     private int[] episodes;
-
+    // 1 - allocated memory space for the object
+    // 2 - each class attribute is created and initialized with default values default or whatever is passed
+    // 3 - initialization block is executed
+    // 4 - constructor is executed
     {
         episodes = new int[100];
         for (int i = 0; i < episodes.length; i++) {
