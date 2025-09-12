5863cb8ccfedb918448a5e64fe4f3a2fad8fb091
fnbritosx <fnbritosx@gmail.com>
2025-09-11 22:03:28 -0300

    feat: improve seminar logic and add student/teacher printer methods

--- marathon-java/devbrito/marathonjava/javacore/association/domain/StudentSeminar.java
+++ marathon-java/devbrito/marathonjava/javacore/association/domain/StudentSeminar.java
@@ -10,7 +10,16 @@ public class StudentSeminar {
         this.age = age;
     }
 
+    public void studentPrinter(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Seminars: " + seminar.getTitle());
+    }
+
     public String getName() {
         return name;
+    }
+    public void setSeminar(Seminar seminar){
+        this.seminar = seminar;
     }
 }
--- marathon-java/devbrito/marathonjava/javacore/association/domain/TeacherSeminar.java
+++ marathon-java/devbrito/marathonjava/javacore/association/domain/TeacherSeminar.java
@@ -6,14 +6,19 @@ import java.util.List;
 public class TeacherSeminar {
     private String name;
     private String specialty;
-    private List<Seminar> seminar = new ArrayList<>();
+    private Seminar seminar;
 
     public TeacherSeminar(String name, String specialty) {
         this.name = name;
         this.specialty = specialty;
     }
 
-    public TeacherSeminar(String name, String specialty, List<Seminar> seminar) {
+    public void teacherPrinter(){
+        System.out.println("Name: " + name);
+        System.out.println("Specialty: " + specialty);
+        System.out.println("Seminar: " + seminar.getTitle());
+    }
+    public TeacherSeminar(String name, String specialty, Seminar seminar) {
         this.name = name;
         this.specialty = specialty;
         this.seminar = seminar;
@@ -23,7 +28,7 @@ public class TeacherSeminar {
         return name;
     }
 
-    public void setSeminar(List<Seminar> seminar){
+    public void setSeminar(Seminar seminar){
         this.seminar = seminar;
     }
 }
