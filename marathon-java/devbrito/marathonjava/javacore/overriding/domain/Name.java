4da61901ea024fadfd1ac9e4295eac28b40cef68
fnbritosx <fnbritosx@gmail.com>
2025-09-14 19:01:21 -0300

    feat: add exercises on method overriding and toString example

---
+++ marathon-java/devbrito/marathonjava/javacore/overriding/domain/Name.java
@@ -0,0 +1,13 @@
+package devbrito.marathonjava.javacore.overriding.domain;
+
+public class Name extends Object {
+    public String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
---
+++ marathon-java/devbrito/marathonjava/javacore/overriding/test/NameTest01.java
@@ -0,0 +1,11 @@
+package devbrito.marathonjava.javacore.overriding.test;
+
+import devbrito.marathonjava.javacore.overriding.domain.Name;
+
+public class NameTest01 {
+    public static void main(String[] args) {
+        Name name = new Name("Fernando");
+
+        System.out.println(name);
+    }
+}
