bb2fd5876b8df3c3b0eb391d3836ae51d93f019c
fnbritosx <fnbritosx@gmail.com>
2025-09-20 21:25:14 -0300

    feat: add checked exception in the code

--- marathon-java/devbrito/marathonjava/javacore/throwable/exception/test/ExceptionTest01.java
+++ marathon-java/devbrito/marathonjava/javacore/throwable/exception/test/ExceptionTest01.java
@@ -4,16 +4,17 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExceptionTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         createNewFile();
     }
-    private static void createNewFile(){
+    private static void createNewFile() throws IOException{
         File file = new File("file/text.txt");
         try {
             boolean isCreate = file.createNewFile();
             System.out.println("File create " + isCreate);
         }catch (IOException e){
             e.printStackTrace();
+            throw e;
         }
     }
 }
