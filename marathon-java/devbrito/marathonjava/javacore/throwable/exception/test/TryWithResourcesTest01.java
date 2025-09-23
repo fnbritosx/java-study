3d609255fe2f8b83d96a857b889393d914ceab54
fnbritosx <fnbritosx@gmail.com>
2025-09-22 21:51:11 -0300

    feat: add exercise on with resources

---
+++ marathon-java/devbrito/marathonjava/javacore/throwable/exception/test/TryWithResourcesTest01.java
@@ -0,0 +1,40 @@
+package devbrito.marathonjava.javacore.throwable.exception.test;
+
+import devbrito.marathonjava.javacore.throwable.exception.domain.Read01;
+import devbrito.marathonjava.javacore.throwable.exception.domain.Read02;
+
+import java.io.*;
+
+public class TryWithResourcesTest01 {
+    public static void main(String[] args) {
+        readFile01();
+    }
+
+    public static void readFile01() {
+//        try (Reader reader = new BufferedReader(new FileReader("text.txt"))) {
+        try (Read01 reader = new Read01();
+             Read02 reader1 = new Read02()) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFile02() {
+        Reader reader02 = null;
+        try {
+            reader02 = new BufferedReader(new FileReader("text.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader02 != null) {
+                    reader02.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
