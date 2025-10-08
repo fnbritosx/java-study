4721d89c762660accf0982320ce04beb92d8a559
fnbritosx <fnbritosx@gmail.com>
2025-10-08 15:51:43 -0300

    feat: add example showing number formatting differences across locales

---
+++ marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/NumberFormatTest01.java
@@ -0,0 +1,24 @@
+package devbrito.marathonjava.javacore.utility_classes.date.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        NumberFormat[] numberFormat = new NumberFormat[3];
+
+        Locale localeBR = Locale.forLanguageTag("pt-BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeCH = Locale.CHINESE;
+
+        numberFormat[0] = NumberFormat.getInstance(localeBR);
+        numberFormat[1] = NumberFormat.getInstance(localeJP);
+        numberFormat[2] = NumberFormat.getInstance(localeCH);
+
+        double value = 100_000.212;
+        for (NumberFormat format : numberFormat) {
+            System.out.println(format.format(value));
+        }
+
+    }
+}
