29225ecb7722d168737b99aa073f41f0b1a682a1
fnbritosx <fnbritosx@gmail.com>
2025-10-10 12:39:46 -0300

    feat: add example of currency formatting and parsing with multiple locales

---
+++ marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/NumberFormatTest02.java
@@ -0,0 +1,35 @@
+package devbrito.marathonjava.javacore.utility_classes.date.test;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class NumberFormatTest02 {
+    public static void main(String[] args) {
+        NumberFormat[] numberFormat = new NumberFormat[4];
+
+        Locale localeBR = Locale.forLanguageTag("pt-BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeCH = Locale.CHINESE;
+        Locale localeIT = Locale.ITALY;
+
+        numberFormat[0] = NumberFormat.getCurrencyInstance(localeBR);
+        numberFormat[1] = NumberFormat.getCurrencyInstance(localeIT);
+        numberFormat[2] = NumberFormat.getCurrencyInstance(localeCH);
+        numberFormat[3] = NumberFormat.getCurrencyInstance(localeJP);
+
+        double value = 100_000.212;
+        for (NumberFormat format : numberFormat) {
+            format.setMaximumFractionDigits(2);
+            System.out.println(format.format(value));
+        }
+
+        String value1 = "R$\u00A0100.000,212";
+
+        try {
+            System.out.println(numberFormat[0].parse(value1));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
