8043222db7621cd922fbcbcc30a5c1e816c702b7
fnbritosx <fnbritosx@gmail.com>
2025-10-14 15:31:34 -0300

    feat: add examples using TemporalAdjusters to get next day, first day of month and year

---
+++ marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/TemporalAdjustersTest01.java
@@ -0,0 +1,18 @@
+package devbrito.marathonjava.javacore.utility_classes.date.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.TUESDAY));
+        System.out.println(now);
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfYear());
+        System.out.println(now.getDayOfWeek());
+    }
+}
