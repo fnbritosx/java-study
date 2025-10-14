5791083195e65094e379048460bd07a191c47a46
fnbritosx <fnbritosx@gmail.com>
2025-10-14 12:47:48 -0300

    feat: add Period tests demonstrating ofDays, ofMonths, ofYears, and ChronoUnit note

--- marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/PeriodTest01.java
+++ marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/PeriodTest01.java
@@ -1,4 +1,26 @@
 package devbrito.marathonjava.javacore.utility_classes.date.test;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class PeriodTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2);
+
+        Period period = Period.between(now, nowAfterTwoYears);
+        Period p1 = Period.ofDays(10);
+        Period p2 = Period.ofMonths(12);
+        Period p3 = Period.ofYears(2005);
+        Period p4 = Period.ofWeeks(60);
+
+
+        System.out.println(period);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4); //print in days
+        System.out.println(p4.getMonths()); // The number of months is zero because weeks are internally converted only into days.
+                                            // to convert to months, use the ChronoUnity class.
+    }
 }
