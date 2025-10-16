be65ffb315eaef65fc00cfb184700cf666f8391a
fnbritosx <fnbritosx@gmail.com>
2025-10-16 13:47:40 -0300

    refactor: move TemporalAdjuster class to a static nested class for better encapsulation

--- marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/ExerciseDate01.java
+++ marathon-java/devbrito/marathonjava/javacore/utility_classes/date/test/ExerciseDate01.java
@@ -1,6 +1,5 @@
 package devbrito.marathonjava.javacore.utility_classes.date.test;
 
-
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
@@ -8,29 +7,28 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
-class newDay implements TemporalAdjuster {
-    @Override
-    public Temporal adjustInto(Temporal temporal) {
-        DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
-        int addDays;
-        switch (dayOfWeek) {
-            case THURSDAY:
-                addDays = 4;
-                break;
-            case FRIDAY:
-                addDays = 3;
-                break;
-            case SATURDAY:
-                addDays = 2;
-                break;
-            default:
-                addDays = 1;
-        }
-        return temporal.plus(addDays, ChronoUnit.DAYS);
-    }
-}
-
 public class ExerciseDate01 {
+    static class newDay implements TemporalAdjuster {
+        @Override
+        public Temporal adjustInto(Temporal temporal) {
+            DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+            int addDays;
+            switch (dayOfWeek) {
+                case THURSDAY:
+                    addDays = 4;
+                    break;
+                case FRIDAY:
+                    addDays = 3;
+                    break;
+                case SATURDAY:
+                    addDays = 2;
+                    break;
+                default:
+                    addDays = 1;
+            }
+            return temporal.plus(addDays, ChronoUnit.DAYS);
+        }
+    }
     public static void main(String[] args) {
 
         LocalDate now = LocalDate.now();
