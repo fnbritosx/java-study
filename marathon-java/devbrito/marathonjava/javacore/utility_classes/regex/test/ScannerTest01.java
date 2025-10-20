2cb6eda6ca3a1c439435cddc6e21abbf6f92d429
fnbritosx <fnbritosx@gmail.com>
2025-10-19 22:01:47 -0300

    feat: add scanner example to parse string tokens into different data types

---
+++ marathon-java/devbrito/marathonjava/javacore/utility_classes/regex/test/ScannerTest01.java
@@ -0,0 +1,23 @@
+package devbrito.marathonjava.javacore.utility_classes.regex.test;
+
+import java.util.Scanner;
+
+public class ScannerTest01 {
+    public static void main(String[] args) {
+        String text = "Eren, Levi, Mikasa, true, 2005";
+        Scanner scanner = new Scanner(text);
+
+        scanner.useDelimiter(", ");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+                int i = scanner.nextInt();
+                System.out.println("Int: " + i);
+            } else if (scanner.hasNextBoolean()) {
+                boolean b = scanner.nextBoolean();
+                System.out.println("Boolean: " + b);
+            } else {
+                System.out.println(scanner.next());
+            }
+        }
+    }
+}
