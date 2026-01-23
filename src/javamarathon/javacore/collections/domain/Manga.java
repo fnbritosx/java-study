4f24f5edac6da654ae9938481a1aa6652a34eacb
Fernando Brito Neto <14994165718@pms.rj.gov.br>
2026-01-23 16:56:48 -0300

    refactor: override equals and hashCode based on id and nome in Manga class

--- src/javamarathon/javacore/collections/domain/Manga.java
+++ src/javamarathon/javacore/collections/domain/Manga.java
@@ -26,6 +26,18 @@ public class Manga implements Comparable<Manga> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Manga manga = (Manga) o;
+        return Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    @Override
     public String toString() {
         return "Manga{" +
                 "id=" + id +
